a6f782b4d6a6f4e47775dfff9a1ef2eae7e09710
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-09-16 21:41:23 +0500

    completed pwa app of water app

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
+function showNotification() {
+  Notification.requestPermission((result) => {
+    if (result === "granted") {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification("Weather notification", {
+          body: "Notification from weather web site",
+          icon: "./images/logo.jpg",
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: "vibration-sample",
+        });
+      });
+    }
+  });
+}
+
+showNotification();
+
+
+
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
 let desc = document.querySelector('.desc');
 let clouds = document.querySelector('.clouds');
-let button= document.querySelector('.submit');
+let button = document.querySelector('.submit');
 
 
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374&units=metric')
-.then(response => response.json())
-.then(data => {
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
- var cen = `<h1> <sup>o</sup>C</h1>`
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue + cen;
-  input.value ="";
+button.addEventListener('click', function (name) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=50a7aa80fa492fa92e874d23ad061374&units=metric')
+    .then(response => response.json())
+    .then(data => {
+      var tempValue = data['main']['temp'];
+      var nameValue = data['name'];
+      var descValue = data['weather'][0]['description'];
+      var cen = `<h1> <sup>o</sup>C</h1>`
+      main.innerHTML = nameValue;
+      desc.innerHTML = "Desc - " + descValue;
+      temp.innerHTML = "Temp - " + tempValue + cen;
+      input.value = "";
 
-})
+    })
 
-.catch(err => alert("Wrong city name!"));
+    .catch(err => alert("Wrong city name!"));
 })
 //  `
 // <img id="temp-icon" src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt=""> `
